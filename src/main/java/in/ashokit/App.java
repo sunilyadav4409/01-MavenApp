546300bca2fbec9b930c05cac075a267eef987b8
sunil <sunilyadav4409@gmail.comclear>
2022-02-19 22:22:25 +0530

    my first commit

---
+++ src/main/java/in/ashokit/App.java
@@ -0,0 +1,14 @@
+package in.ashokit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Loginpage l=new Loginpage();
+        l.show();
+    }
+}
---
+++ src/main/java/in/ashokit/Loginpage.java
@@ -0,0 +1,9 @@
+package in.ashokit;
+
+public class Loginpage {
+	public void show()
+	{
+		System.out.println("login credentials");
+	}
+
+}
