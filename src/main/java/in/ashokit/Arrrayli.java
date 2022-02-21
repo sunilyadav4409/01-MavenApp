2e53d5e435cb54a4cdf2ed458d0ca0d67426b811
sunil <sunilyadav4409@gmail.comclear>
2022-02-21 07:35:26 +0530

    adding arraylist and linked list

---
+++ src/main/java/in/ashokit/Arrrayli.java
@@ -0,0 +1,15 @@
+package in.ashokit;
+
+import java.util.ArrayList;
+
+public class Arrrayli {
+ public static void main(String[] args) {
+	ArrayList l=new ArrayList();
+	l.add(99);
+	l.add(null);
+	l.add("sunilyadav");
+	l.add('s');
+	l.add(99);
+	System.out.println(l);
+}
+}
---
+++ src/main/java/in/ashokit/Linked.java
@@ -0,0 +1,18 @@
+package in.ashokit;
+
+import java.util.LinkedList;
+
+public class Linked {
+
+	public static void main(String[] args) {
+     LinkedList l=new LinkedList();
+     l.add(78);
+     l.add("sunilyadav");
+     l.add(null);
+     l.add(78);
+     l.add(78);
+     l.add('s');
+     System.out.println(l);
+	}
+
+}
